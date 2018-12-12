d045f24b7d0d8229b19c32696f1c1e3a8fa3221a
Orly Kierszenbaum <orlykierszenbaum@gmail.com>
2018-12-12 16:32:22 +0200

    some changes

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
 from paths.view import View
 
-v = View()
+# v = View()
 #  v.start()
-v.draw_paths()
+# v.draw_paths()
+filters = {
+            1: 'paths by time',
+            2: 'paths by date and time',
+            3: 'paths per location',
+            4: 'paths per location and time',
+            5: 'paths per location, date and time'
+        }
+msg = """
+            1: paths by time
+            2: paths by date and time
+            3: paths per location
+            4: paths per location and time
+            5: paths per location, date and time
+        """
+
+inp = input(f'to choose your filter press :{msg}\n ')
+
+if filters.get(inp) is None:
+    switch_cases()
+
+print('The result for inp is : ', filters.get(inp))
--- paths/view.py
+++ paths/view.py
@@ -6,8 +6,10 @@ class View:
 
     def __init__(self):
         self.cone = c.Controller()
-        self.file_name = input("Please enter the file name")
-        self.im_name = input("Please enter the image name or enter to use default image")
+        # self.file_name = input("Please enter the file name")
+        # self.im_name = input("Please enter the image name or enter to use default image")
+        self.file_name = "data/fixed.csv"
+        self.im_name = "data/paths0.png"
         if not self.im_name:
             self.im_name = "data/paths0.png"
         try:
@@ -20,6 +22,33 @@ class View:
             print("Can't read the image")
 
         self.cone.draw_paths()
+
+
+    def switch_cases(self):
+        x = None
+
+        while(x is None):
+
+           filters = {
+               1: 'paths by time',
+               2: 'paths by date and time',
+               3: 'paths per location',
+               4: 'paths per location and time',
+               5: 'paths per location, date and time'
+           }
+           msg = """
+                       1: paths by time
+                       2: paths by date and time
+                       3: paths per location
+                       4: paths per location and time
+                       5: paths per location, date and time
+                   """
+
+           inp = input(f'to choose your filter press :{msg}\n ')
+           x = filters.get(inp)
+        return filters[x]
+
+
 
 
 
